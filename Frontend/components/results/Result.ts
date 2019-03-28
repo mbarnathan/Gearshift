e48703da362756143af7580b57f0ca1722012e1b
Michael Barnathan <michael@barnathan.net>
2019-03-28 05:54:28 -0700

    Highlighting.

--- Frontend/components/results/Result.ts
+++ Frontend/components/results/Result.ts
@@ -1,5 +1,7 @@
 import {BaseResult} from "./BaseResult";
 import * as _ from "lodash";
+import * as $ from "jquery";
+import hyper from "hyperhtml";
 
 export class Result extends BaseResult {
   public _service?: string;
@@ -38,10 +40,25 @@ export class Result extends BaseResult {
   public navigateUp():boolean { return this.navigate(() => null, () => null); }
 
   public render() {
+    let query = $("#search").val();
+    let name = [this.name];
+    if (query) {
+      // TODO(mb): Don't want to recreate this regex over and over.
+      let regex = new RegExp(`(${query})`, "igu");
+      let groups = this.name.split(regex);
+      for (let group_idx in groups) {
+        if (groups[group_idx].toLocaleLowerCase() == query.toString().toLocaleLowerCase()) {
+          groups[group_idx] = hyper`<span class="match">${groups[group_idx]}</span>`;
+        } else {
+          groups[group_idx] = hyper`${groups[group_idx]}`;
+        }
+      }
+      name = groups;
+    }
     return this.html`
         <tr class="${this.focused() ? "focused" : "unfocused"}">
           <td class="thumbnail"><img src="${this.icon()}" alt="" /></td>
-          <td class="filename"><span class="name">${this.name}</span> <span class="path">(${this.path})</span></td>
+          <td class="filename"><span class="name">${name}</span> <span class="path">(${this.path})</span></td>
           <td><time>${this.modified.toLocaleString()}</time></td>
           <td>${this.size}</td>
           <td class="rightcol">
